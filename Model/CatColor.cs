447350ea09db706083d91a27d6fc7b01f7ca0863
NoFearOfTheCode <xpaacan4ik@mail.ru>
2015-10-21 10:21:55 +0700

    Update CatColor.cs

--- Model/CatColor.cs
+++ Model/CatColor.cs
@@ -2,13 +2,7 @@
 {
     public class CatColor
     {
-        /// <summary>
-        /// read/write property.
-        /// </summary>
         public string HeathyColor { get; set; }
-        /// <summary>
-        /// read/write property.
-        /// </summary>
         public string SickColor { get; set; }
     }
 }
